3cd3f7956b13cffae02d35d66ad6d1600b11b230
Stephen Levine <stephen.levine@gmail.com>
2018-06-03 16:11:25 -0400

    Split out builder tests

--- builder_test.go
+++ builder_test.go
@@ -6,78 +6,50 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"reflect"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/sclevine/spec"
 	"github.com/sclevine/spec/report"
 
+	"reflect"
+
 	"github.com/sclevine/lifecycle"
 	"github.com/sclevine/lifecycle/testmock"
 )
 
 func TestBuilder(t *testing.T) {
-	spec.Run(t, "#Build", testBuild, spec.Report(report.Terminal{}))
+	spec.Run(t, "Builder", testBuilder, spec.Report(report.Terminal{}))
 }
 
 //go:generate mockgen -package mocks -destination testmock/env.go github.com/sclevine/lifecycle Env
 
-func testBuild(t *testing.T, _ spec.G, it spec.S) {
-	// TODO: break into smaller tests
-	it("builds an app", func() {
-		tmpDir, err := ioutil.TempDir("", "test.lifecycle")
+func testBuilder(t *testing.T, when spec.G, it spec.S) {
+	var (
+		builder        *lifecycle.Builder
+		env            *testmock.MockEnv
+		mockCtrl       *gomock.Controller
+		tmpDir         string
+		platformDir    string
+		stdout, stderr *bytes.Buffer
+	)
+
+	it.Before(func() {
+		var err error
+		tmpDir, err = ioutil.TempDir("", "lifecycle")
 		if err != nil {
 			t.Fatalf("Error: %s\n", err)
 		}
-		defer os.RemoveAll(tmpDir)
+		platformDir = filepath.Join(tmpDir, "platform")
+		mkdirs(t, filepath.Join(platformDir, "env"))
 
-		cacheDir := filepath.Join(tmpDir, "cache")
-		launchDir := filepath.Join(tmpDir, "launch")
-		appDir := filepath.Join(launchDir, "app")
-		platformDir := filepath.Join(tmpDir, "platform")
-		mkdirs(t,
-			filepath.Join(cacheDir, "buildpack1-id"),
-			filepath.Join(launchDir, "buildpack1-id"),
-
-			filepath.Join(appDir, "cache-buildpack1", "cache-layer1"),
-			filepath.Join(appDir, "cache-buildpack1", "cache-layer2"),
-			filepath.Join(appDir, "cache-buildpack2", "cache-layer3"),
-			filepath.Join(appDir, "cache-buildpack2", "cache-layer4"),
-
-			filepath.Join(appDir, "launch-buildpack1", "launch-layer1"),
-			filepath.Join(appDir, "launch-buildpack2", "launch-layer2"),
-
-			filepath.Join(platformDir, "env"),
-		)
-		mkfiles(t, "some-data",
-			filepath.Join(platformDir, "env", "SOME_VAR"),
-		)
-
-		mockCtrl := gomock.NewController(t)
-		defer mockCtrl.Finish()
-		env := testmock.NewMockEnv(mockCtrl)
-
+		mockCtrl = gomock.NewController(t)
+		env = testmock.NewMockEnv(mockCtrl)
 		env.EXPECT().List().Return([]string{"ID=1"})
 		env.EXPECT().List().Return([]string{"ID=2"})
-		gomock.InOrder(
-			env.EXPECT().AppendDirs(filepath.Join(cacheDir, "buildpack1-id", "cache-layer1")),
-			env.EXPECT().AppendDirs(filepath.Join(cacheDir, "buildpack1-id", "cache-layer2")),
-			env.EXPECT().SetEnvDir(filepath.Join(cacheDir, "buildpack1-id", "cache-layer1", "env", "set")),
-			env.EXPECT().SetEnvDir(filepath.Join(cacheDir, "buildpack1-id", "cache-layer2", "env", "set")),
-			env.EXPECT().AddEnvDir(filepath.Join(cacheDir, "buildpack1-id", "cache-layer1", "env", "add")),
-			env.EXPECT().AddEnvDir(filepath.Join(cacheDir, "buildpack1-id", "cache-layer2", "env", "add")),
 
-			env.EXPECT().AppendDirs(filepath.Join(cacheDir, "buildpack2-id", "cache-layer3")),
-			env.EXPECT().AppendDirs(filepath.Join(cacheDir, "buildpack2-id", "cache-layer4")),
-			env.EXPECT().SetEnvDir(filepath.Join(cacheDir, "buildpack2-id", "cache-layer3", "env", "set")),
-			env.EXPECT().SetEnvDir(filepath.Join(cacheDir, "buildpack2-id", "cache-layer4", "env", "set")),
-			env.EXPECT().AddEnvDir(filepath.Join(cacheDir, "buildpack2-id", "cache-layer3", "env", "add")),
-			env.EXPECT().AddEnvDir(filepath.Join(cacheDir, "buildpack2-id", "cache-layer4", "env", "add")),
-		)
-
-		stdout, stderr := &bytes.Buffer{}, &bytes.Buffer{}
-		builder := lifecycle.Builder{
+		stdout, stderr = &bytes.Buffer{}, &bytes.Buffer{}
+		builder = &lifecycle.Builder{
 			PlatformDir: platformDir,
 			Buildpacks: lifecycle.BuildpackGroup{
 				{ID: "buildpack1-id", Name: "buildpack1-name", Dir: filepath.Join("testdata", "buildpack")},
@@ -85,31 +57,119 @@ func testBuild(t *testing.T, _ spec.G, it spec.S) {
 			},
 			Out: io.MultiWriter(stdout, it.Out()), Err: io.MultiWriter(stderr, it.Out()),
 		}
-		metadata, err := builder.Build(appDir, launchDir, cacheDir, env)
-		if err != nil {
-			t.Fatalf("Error: %s\n", err)
-		}
-		if !reflect.DeepEqual(metadata, &lifecycle.BuildMetadata{
-			Processes: []lifecycle.Process{
-				{Type: "override-type", Command: "process2-command"},
-				{Type: "process1-type", Command: "process1-command"},
-				{Type: "process2-type", Command: "process2-command"},
-			},
-		}) {
-			t.Fatalf("Unexpected:\n%+v\n", metadata)
-		}
-		if stdout.String() != "STDOUT1\nSTDOUT2\n" {
-			t.Fatalf("Unexpected: %s", stdout)
-		}
-		if stderr.String() != "STDERR1\nSTDERR2\n" {
-			t.Fatalf("Unexpected: %s", stderr)
-		}
-		testExists(t,
-			filepath.Join(launchDir, "buildpack1-id", "launch-layer1"),
-			filepath.Join(launchDir, "buildpack2-id", "launch-layer2"),
-			filepath.Join(appDir, "env-buildpack1", "SOME_VAR"),
-			filepath.Join(appDir, "env-buildpack2", "SOME_VAR"),
+	})
+
+	it.After(func() {
+		os.RemoveAll(tmpDir)
+		mockCtrl.Finish()
+	})
+
+	when("#Build", func() {
+		var (
+			appDir    string
+			launchDir string
+			cacheDir  string
 		)
+
+		it.Before(func() {
+			cacheDir = filepath.Join(tmpDir, "cache")
+			launchDir = filepath.Join(tmpDir, "launch")
+			appDir = filepath.Join(launchDir, "app")
+			mkdirs(t, cacheDir, launchDir, appDir)
+		})
+
+		it("should ensure each cache dir exists and process it", func() {
+			mkdirs(t,
+				filepath.Join(cacheDir, "buildpack1-id"),
+
+				filepath.Join(appDir, "cache-buildpack1", "cache-layer1"),
+				filepath.Join(appDir, "cache-buildpack1", "cache-layer2"),
+				filepath.Join(appDir, "cache-buildpack2", "cache-layer3"),
+				filepath.Join(appDir, "cache-buildpack2", "cache-layer4"),
+			)
+
+			gomock.InOrder(
+				env.EXPECT().AppendDirs(filepath.Join(cacheDir, "buildpack1-id", "cache-layer1")),
+				env.EXPECT().AppendDirs(filepath.Join(cacheDir, "buildpack1-id", "cache-layer2")),
+				env.EXPECT().SetEnvDir(filepath.Join(cacheDir, "buildpack1-id", "cache-layer1", "env", "set")),
+				env.EXPECT().SetEnvDir(filepath.Join(cacheDir, "buildpack1-id", "cache-layer2", "env", "set")),
+				env.EXPECT().AddEnvDir(filepath.Join(cacheDir, "buildpack1-id", "cache-layer1", "env", "add")),
+				env.EXPECT().AddEnvDir(filepath.Join(cacheDir, "buildpack1-id", "cache-layer2", "env", "add")),
+
+				env.EXPECT().AppendDirs(filepath.Join(cacheDir, "buildpack2-id", "cache-layer3")),
+				env.EXPECT().AppendDirs(filepath.Join(cacheDir, "buildpack2-id", "cache-layer4")),
+				env.EXPECT().SetEnvDir(filepath.Join(cacheDir, "buildpack2-id", "cache-layer3", "env", "set")),
+				env.EXPECT().SetEnvDir(filepath.Join(cacheDir, "buildpack2-id", "cache-layer4", "env", "set")),
+				env.EXPECT().AddEnvDir(filepath.Join(cacheDir, "buildpack2-id", "cache-layer3", "env", "add")),
+				env.EXPECT().AddEnvDir(filepath.Join(cacheDir, "buildpack2-id", "cache-layer4", "env", "add")),
+			)
+
+			_, err := builder.Build(appDir, launchDir, cacheDir, env)
+			if err != nil {
+				t.Fatalf("Error: %s\n", err)
+			}
+		})
+
+		it("should ensure each launch dir exists and process it", func() {
+			mkdirs(t,
+				filepath.Join(launchDir, "buildpack1-id"),
+
+				filepath.Join(appDir, "launch-buildpack1", "launch-layer1"),
+				filepath.Join(appDir, "launch-buildpack2", "launch-layer2"),
+			)
+
+			_, err := builder.Build(appDir, launchDir, cacheDir, env)
+			if err != nil {
+				t.Fatalf("Error: %s\n", err)
+			}
+			testExists(t,
+				filepath.Join(launchDir, "buildpack1-id", "launch-layer1"),
+				filepath.Join(launchDir, "buildpack2-id", "launch-layer2"),
+			)
+		})
+
+		it("should return launch metadata", func() {
+			metadata, err := builder.Build(appDir, launchDir, cacheDir, env)
+			if err != nil {
+				t.Fatalf("Error: %s\n", err)
+			}
+			if !reflect.DeepEqual(metadata, &lifecycle.BuildMetadata{
+				Processes: []lifecycle.Process{
+					{Type: "override-type", Command: "process2-command"},
+					{Type: "process1-type", Command: "process1-command"},
+					{Type: "process2-type", Command: "process2-command"},
+				},
+			}) {
+				t.Fatalf("Unexpected:\n%+v\n", metadata)
+			}
+		})
+
+		it("should provide the platform dir", func() {
+			mkfiles(t, "some-data",
+				filepath.Join(platformDir, "env", "SOME_VAR"),
+			)
+			_, err := builder.Build(appDir, launchDir, cacheDir, env)
+			if err != nil {
+				t.Fatalf("Error: %s\n", err)
+			}
+			testExists(t,
+				filepath.Join(appDir, "env-buildpack1", "SOME_VAR"),
+				filepath.Join(appDir, "env-buildpack2", "SOME_VAR"),
+			)
+		})
+
+		it("should connect stdout and stdin to the terminal", func() {
+			_, err := builder.Build(appDir, launchDir, cacheDir, env)
+			if err != nil {
+				t.Fatalf("Error: %s\n", err)
+			}
+			if stdout.String() != "STDOUT1\nSTDOUT2\n" {
+				t.Fatalf("Unexpected: %s", stdout)
+			}
+			if stderr.String() != "STDERR1\nSTDERR2\n" {
+				t.Fatalf("Unexpected: %s", stderr)
+			}
+		})
 	})
 }
 
