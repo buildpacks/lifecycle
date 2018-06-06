8ed8b3874b90b6d5c30fb615d15e0bdc2abe4e5d
Stephen Levine <stephen.levine@gmail.com>
2018-06-06 01:00:45 -0400

    Rename env file

--- env.go
+++ env.go
@@ -6,30 +6,6 @@ import (
 	"path/filepath"
 )
 
-var POSIXBuildEnv = map[string][]string{
-	"bin": {
-		"PATH",
-	},
-	"lib": {
-		"LD_LIBRARY_PATH",
-		"LIBRARY_PATH",
-	},
-	"include": {
-		"CPATH",
-		"C_INCLUDE_PATH",
-		"CPLUS_INCLUDE_PATH",
-		"OBJC_INCLUDE_PATH",
-	},
-	"pkgconfig": {
-		"PKG_CONFIG_PATH",
-	},
-}
-
-var POSIXLaunchEnv = map[string][]string{
-	"bin": {"PATH"},
-	"lib": {"LD_LIBRARY_PATH"},
-}
-
 type Env struct {
 	Getenv  func(key string) string
 	Setenv  func(key, value string) error
--- lifecycle.go
+++ lifecycle.go
@@ -6,3 +6,27 @@ const (
 	DefaultLaunchDir   = "/launch"
 	DefaultAppDir      = DefaultLaunchDir + "/app"
 )
+
+var POSIXBuildEnv = map[string][]string{
+	"bin": {
+		"PATH",
+	},
+	"lib": {
+		"LD_LIBRARY_PATH",
+		"LIBRARY_PATH",
+	},
+	"include": {
+		"CPATH",
+		"C_INCLUDE_PATH",
+		"CPLUS_INCLUDE_PATH",
+		"OBJC_INCLUDE_PATH",
+	},
+	"pkgconfig": {
+		"PKG_CONFIG_PATH",
+	},
+}
+
+var POSIXLaunchEnv = map[string][]string{
+	"bin": {"PATH"},
+	"lib": {"LD_LIBRARY_PATH"},
+}
