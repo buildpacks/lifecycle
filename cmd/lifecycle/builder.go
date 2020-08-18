e61d14ddf6bd1965b48e2cc62932ac55751713fa
Joe Kutner <jpkutner@gmail.com>
2020-08-18 08:11:11 -0500

    This change introduces a stack buildpack that is identified by a privileged key in the buildpack.toml When the privileged key is set to true, the builder will ignore layers created in the layers directory and instead will take a snapshot of the changes made during execution of the stack buildpack.
    Signed-off-by: Joe Kutner <jpkutner@gmail.com>

--- cmd/lifecycle/builder.go
+++ cmd/lifecycle/builder.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
 
@@ -13,17 +14,20 @@ import (
 	"github.com/buildpacks/lifecycle/env"
 	"github.com/buildpacks/lifecycle/launch"
 	"github.com/buildpacks/lifecycle/priv"
+	"github.com/buildpacks/lifecycle/snapshot"
 )
 
 type buildCmd struct {
 	// flags: inputs
-	groupPath string
-	planPath  string
+	groupPath      string
+	stackGroupPath string
+	planPath       string
 	buildArgs
 }
 
 type buildArgs struct {
 	// inputs needed when run by creator
+	uid, gid      int
 	buildpacksDir string
 	layersDir     string
 	appDir        string
@@ -38,6 +42,7 @@ func (b *buildCmd) Init() {
 	cmd.FlagLayersDir(&b.layersDir)
 	cmd.FlagAppDir(&b.appDir)
 	cmd.FlagPlatformDir(&b.platformDir)
+	cmd.FlagStackGroupPath(&b.stackGroupPath)
 }
 
 func (b *buildCmd) Args(nargs int, args []string) error {
@@ -48,25 +53,63 @@ func (b *buildCmd) Args(nargs int, args []string) error {
 }
 
 func (b *buildCmd) Privileges() error {
-	// builder should never be run with privileges
-	if priv.IsPrivileged() {
+	// builder should never be run with privileges if there aren't any stack buildpacks
+	if b.stackGroupPath == "" && priv.IsPrivileged() {
 		return cmd.FailErr(errors.New("refusing to run as root"), "build")
 	}
 	return nil
 }
 
 func (b *buildCmd) Exec() error {
-	group, plan, err := b.readData()
+	group, stackGroup, plan, err := b.readData()
 	if err != nil {
 		return err
 	}
 	if err := verifyBuildpackApis(group); err != nil {
 		return err
 	}
-	return b.build(group, plan)
+
+	if err = b.buildWithSnapshot(stackGroup, plan); err != nil {
+		return err
+	}
+
+	if err = b.buildWithLayers(group, plan); err != nil {
+		return err
+	}
+	return nil
 }
 
-func (ba buildArgs) build(group lifecycle.BuildpackGroup, plan lifecycle.BuildPlan) error {
+func (ba buildArgs) buildWithSnapshot(group lifecycle.BuildpackGroup, plan lifecycle.BuildPlan) error {
+	// run stack buildpacks as root
+	stackSnapshotter, err := snapshot.NewKanikoSnapshotter("/")
+	if err != nil {
+		return err
+	}
+	if _, err = ba.build(group, plan, stackSnapshotter.RootDir, stackSnapshotter); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ba buildArgs) buildWithLayers(group lifecycle.BuildpackGroup, plan lifecycle.BuildPlan) error {
+	// drop back to non-root user to run buildpacks
+	if err := priv.RunAs(ba.uid, ba.gid); err != nil {
+		return cmd.FailErr(err, fmt.Sprintf("exec as user %d:%d", ba.uid, ba.gid))
+	}
+	if err := priv.SetEnvironmentForUser(ba.uid); err != nil {
+		return cmd.FailErr(err, fmt.Sprintf("set environment for user %d", ba.uid))
+	}
+	md, err := ba.build(group, plan, ba.appDir, &lifecycle.NoopSnapshotter{})
+	if err != nil {
+		return err
+	}
+	if err := lifecycle.WriteTOML(launch.GetMetadataFilePath(ba.layersDir), md); err != nil {
+		return cmd.FailErr(err, "write build metadata")
+	}
+	return nil
+}
+
+func (ba buildArgs) build(group lifecycle.BuildpackGroup, plan lifecycle.BuildPlan, baseDir string, snapshotter lifecycle.LayerSnapshotter) (*lifecycle.BuildMetadata, error) {
 	builder := &lifecycle.Builder{
 		AppDir:        ba.appDir,
 		LayersDir:     ba.layersDir,
@@ -78,33 +121,42 @@ func (ba buildArgs) build(group lifecycle.BuildpackGroup, plan lifecycle.BuildPl
 		Plan:          plan,
 		Out:           log.New(os.Stdout, "", 0),
 		Err:           log.New(os.Stderr, "", 0),
+		Snapshotter:   snapshotter,
 	}
 	md, err := builder.Build()
 
 	if err != nil {
 		if err, ok := err.(*lifecycle.Error); ok {
 			if err.Type == lifecycle.ErrTypeBuildpack {
-				return cmd.FailErrCode(err.Cause(), cmd.CodeFailedBuildWithErrors, "build")
+				return md, cmd.FailErrCode(err.Cause(), cmd.CodeFailedBuildWithErrors, "build")
 			}
 		}
-		return cmd.FailErrCode(err, cmd.CodeBuildError, "build")
+		return md, cmd.FailErrCode(err, cmd.CodeBuildError, "build")
 	}
 
-	if err := lifecycle.WriteTOML(launch.GetMetadataFilePath(ba.layersDir), md); err != nil {
-		return cmd.FailErr(err, "write build metadata")
-	}
-	return nil
+	return md, nil
 }
 
-func (b *buildCmd) readData() (lifecycle.BuildpackGroup, lifecycle.BuildPlan, error) {
-	group, err := lifecycle.ReadGroup(b.groupPath)
-	if err != nil {
-		return lifecycle.BuildpackGroup{}, lifecycle.BuildPlan{}, cmd.FailErr(err, "read buildpack group")
+func (b *buildCmd) readData() (lifecycle.BuildpackGroup, lifecycle.BuildpackGroup, lifecycle.BuildPlan, error) {
+	group := lifecycle.BuildpackGroup{}
+	if _, err := os.Stat(b.groupPath); err == nil {
+		group, err = lifecycle.ReadGroup(b.groupPath)
+		if err != nil {
+			return lifecycle.BuildpackGroup{}, lifecycle.BuildpackGroup{}, lifecycle.BuildPlan{}, cmd.FailErr(err, "read buildpack group")
+		}
+	}
+
+	stackGroup := lifecycle.BuildpackGroup{}
+	if _, err := os.Stat(b.stackGroupPath); err == nil {
+		stackGroup, err = lifecycle.ReadGroup(b.stackGroupPath)
+		if err != nil {
+			return group, lifecycle.BuildpackGroup{}, lifecycle.BuildPlan{}, cmd.FailErr(err, "read stack buildpack group")
+		}
 	}
 
 	var plan lifecycle.BuildPlan
 	if _, err := toml.DecodeFile(b.planPath, &plan); err != nil {
-		return lifecycle.BuildpackGroup{}, lifecycle.BuildPlan{}, cmd.FailErr(err, "parse detect plan")
+		return lifecycle.BuildpackGroup{}, lifecycle.BuildpackGroup{}, lifecycle.BuildPlan{}, cmd.FailErr(err, "parse detect plan")
 	}
-	return group, plan, nil
+	return group, stackGroup, plan, nil
 }
--- tools/go.mod
+++ tools/go.mod
@@ -12,4 +12,9 @@ require (
 	rsc.io/quote/v3 v3.1.0 // indirect
 )
 
-replace github.com/buildpacks/lifecycle v0.8.1 => ../
+replace (
+	github.com/buildpacks/lifecycle v0.8.1 => ../
+	github.com/containerd/containerd v1.4.0-0.20191014053712-acdcf13d5eaf => github.com/containerd/containerd v0.0.0-20191014053712-acdcf13d5eaf
+	github.com/docker/docker v1.14.0-0.20190319215453-e7b5f7dbe98c => github.com/docker/docker v1.4.2-0.20200221181110-62bd5a33f707
+	github.com/tonistiigi/fsutil v0.0.0-20190819224149-3d2716dd0a4d => github.com/tonistiigi/fsutil v0.0.0-20191018213012-0f039a052ca1
+)
