6d9e212db83fb960a5b7d5e56e2f6e72550121cc
Jesse Brown <jabrown85@gmail.com>
2020-09-10 10:16:48 -0500

    Forgot to read id/version for windows test detect.bat
    Signed-off-by: Jesse Brown <jabrown85@gmail.com>

--- testdata/stackpacks/stack_a/v1/bin/detect.bat
+++ testdata/stackpacks/stack_a/v1/bin/detect.bat
@@ -4,6 +4,14 @@ set plan_path=%3
 
 set bp_dir=%~dp0..
 
+for /f "tokens=* USEBACKQ" %%F in (`type %bp_dir%\buildpack.toml ^| yj -t ^| jq -r .buildpack.id`) do (
+  set bp_id=%%F
+)
+
+for /f "tokens=* USEBACKQ" %%F in (`type %bp_dir%\buildpack.toml ^| yj -t ^| jq -r .buildpack.version`) do (
+  set bp_version=%%F
+)
+
 if exist detect-status-%bp_id%-%bp_version% (
   for /f "tokens=* USEBACKQ" %%F in (`type detect-status-%bp_id%-%bp_version%`) do (
     exit /b %%F
