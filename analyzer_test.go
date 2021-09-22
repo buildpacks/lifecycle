819a998c4ab12ff5ebca07cb95f23c6ee6f35ac3
Natalie Arellano <narellano@vmware.com>
2021-09-22 12:00:06 -0400

    Test relies on platform to provide the ops
    Signed-off-by: Natalie Arellano <narellano@vmware.com>

--- analyzer_test.go
+++ analyzer_test.go
@@ -21,7 +21,6 @@ import (
 	"github.com/buildpacks/lifecycle/cache"
 	"github.com/buildpacks/lifecycle/cmd"
 	"github.com/buildpacks/lifecycle/cmd/lifecycle/platform"
-	"github.com/buildpacks/lifecycle/cmd/lifecycle/platform/pre06"
 	dataformat "github.com/buildpacks/lifecycle/platform"
 	h "github.com/buildpacks/lifecycle/testhelpers"
 	"github.com/buildpacks/lifecycle/testmock"
@@ -86,7 +85,7 @@ func testAnalyzerBuilder(platformAPI string) func(t *testing.T, when spec.G, it 
 				LayerMetadataRestorer: metadataRestorer,
 			}
 
-			ops = append(ops, pre06.ReadPreviousImage)
+			ops = p.AnalyzeOperations()
 
 			if testing.Verbose() {
 				analyzer.Logger = cmd.DefaultLogger
@@ -111,7 +110,6 @@ func testAnalyzerBuilder(platformAPI string) func(t *testing.T, when spec.G, it 
 
 			expectRestoresLayerMetadataIfSupported := func() {
 				if api.MustParse(analyzer.Platform.API()).LessThan("0.7") {
-					ops = append(ops, pre06.RestoreLayerMetadata)
 					useShaFiles := true
 					layerSHAStore := lifecycle.NewLayerSHAStore(useShaFiles)
 					metadataRestorer.EXPECT().Restore(analyzer.Buildpacks, expectedAppMetadata, expectedCacheMetadata, layerSHAStore)
