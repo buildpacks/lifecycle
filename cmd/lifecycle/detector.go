b8f029f0d7a50c85381d48113ff8e141de905c75
Natalie Arellano <narellano@vmware.com>
2023-03-01 13:31:09 -0500

    Don't redefine -layers
    Signed-off-by: Natalie Arellano <narellano@vmware.com>

--- cmd/lifecycle/detector.go
+++ cmd/lifecycle/detector.go
@@ -32,7 +32,6 @@ func (d *detectCmd) DefineFlags() {
 	cli.FlagAppDir(&d.AppDir)
 	cli.FlagBuildpacksDir(&d.BuildpacksDir)
 	cli.FlagGroupPath(&d.GroupPath)
-	cli.FlagLayersDir(&d.LayersDir)
 	cli.FlagOrderPath(&d.OrderPath)
 	cli.FlagPlanPath(&d.PlanPath)
 	cli.FlagPlatformDir(&d.PlatformDir)
