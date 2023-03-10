86647cb5f8163bf964ccea42ee13d46210e859bc
Jesse Brown <jabrown85@gmail.com>
2023-03-10 11:04:44 -0600

    fixup! Read rebase from io.buildpacks.lifecycle.metdata runImage
    Signed-off-by: Jesse Brown <jabrown85@gmail.com>

--- cmd/lifecycle/rebaser.go
+++ cmd/lifecycle/rebaser.go
@@ -163,15 +163,19 @@ func (r *rebaseCmd) setAppImage() error {
 
 	if r.RunImageRef == "" {
 		if r.PlatformAPI.AtLeast("0.12") {
-			if md.RunImage.Reference == "" {
-				return cmd.FailErrCode(errors.New("-run-image is required when there is no run image metadata available"), cmd.CodeForInvalidArgs, "parse arguments")
-			}
 			r.RunImageRef = md.RunImage.Reference
-			return nil
+			if r.RunImageRef != "" {
+				return nil
+			}
 		}
+
+		// for backwards compatibility, we need to fallback to the stack metadata
+		// fail if there is no run image metadata available from either location
 		if md.Stack.RunImage.Image == "" {
-			return cmd.FailErrCode(errors.New("-run-image is required when there is no stack metadata available"), cmd.CodeForInvalidArgs, "parse arguments")
+			return cmd.FailErrCode(errors.New("-run-image is required when there is no run image metadata available"), cmd.CodeForInvalidArgs, "parse arguments")
 		}
+
+		// for older platforms, we find the best mirror for the run image as this point
 		r.RunImageRef, err = md.Stack.BestRunImageMirror(registry)
 		if err != nil {
 			return err
