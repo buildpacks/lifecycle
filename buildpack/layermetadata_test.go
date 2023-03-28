ef1e4d428cc149624b0f9880da7df2662ec83f01
anushkamittal20 <anumittal4641@gmail.com>
2023-03-28 17:50:45 +0530

    checks in layermetadata_test.go for logger
    Signed-off-by: anushkamittal20 <anumittal4641@gmail.com>

--- buildpack/layermetadata_test.go
+++ buildpack/layermetadata_test.go
@@ -1,10 +1,15 @@
 package buildpack_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
-	"github.com/buildpacks/lifecycle/log"
+	"github.com/apex/log"
+
+	"github.com/apex/log/handlers/memory"
+
+	llog "github.com/buildpacks/lifecycle/log"
 	"github.com/sclevine/spec"
 	"github.com/sclevine/spec/report"
 
@@ -12,7 +17,8 @@ import (
 	h "github.com/buildpacks/lifecycle/testhelpers"
 )
 
-var logger log.Logger
+var logHandler *memory.Handler
+var logger llog.Logger
 
 func TestLayerMetadata(t *testing.T) {
 	spec.Run(t, "unit-layermetadata", testLayerMetadata, spec.Report(report.Terminal{}))
@@ -27,6 +33,8 @@ func testLayerMetadata(t *testing.T, when spec.G, it spec.S) {
 			var err error
 			metadataFile, err = os.CreateTemp("", "test")
 			h.AssertNil(t, err)
+			logHandler = memory.New()
+			logger = &log.Logger{Handler: logHandler}
 		})
 		it.After(func() {
 			os.Remove(metadataFile.Name())
@@ -41,7 +49,19 @@ func testLayerMetadata(t *testing.T, when spec.G, it spec.S) {
 			h.AssertEq(t, lmf.Cache, true)
 			h.AssertEq(t, lmf.Build, false)
 			h.AssertEq(t, lmf.Launch, false)
-			// add assertLogEntry : 	assertLogEntry(t, logHandler, expected)
+		})
+		it("metadata file in wrong format", func() {
+			err := os.WriteFile(metadataFile.Name(), []byte("cache = true"), 0400)
+			h.AssertNil(t, err)
+
+			var lmf buildpack.LayerMetadataFile
+			lmf, err = buildpack.DecodeLayerMetadataFile(metadataFile.Name(), "0.9", logger)
+			h.AssertNil(t, err)
+			h.AssertEq(t, lmf.Cache, false)
+			h.AssertEq(t, lmf.Build, false)
+			h.AssertEq(t, lmf.Launch, false)
+			expected := fmt.Sprintf("the launch, cache and build flags should be in the types table of %s", metadataFile.Name())
+			h.AssertLogEntry(t, logHandler, expected)
 		})
 	})
 }
