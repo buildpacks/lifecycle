c457a229aa04f2105b088e80a24a0df838cb811b
Ralf Pannemans <ralf.pannemans@googlemail.com>
2023-05-10 08:00:51 +0200

    Update acceptance/analyzer_test.go
    Co-authored-by: Natalie Arellano <narellano@vmware.com>
    Signed-off-by: Ralf Pannemans <ralf.pannemans@googlemail.com>

--- acceptance/analyzer_test.go
+++ acceptance/analyzer_test.go
@@ -47,8 +47,8 @@ func TestAnalyzer(t *testing.T) {
 	analyzeDaemonFixtures = analyzeTest.targetDaemon.fixtures
 	analyzeRegFixtures = analyzeTest.targetRegistry.fixtures
 
-	for _, platformAPI := range []string{"0.12"} {
-		spec.Run(t, "acceptance-analyzer/"+platformAPI, testAnalyzerFunc(platformAPI), spec.Parallel(), spec.Report(report.Terminal{}))
+	for _, platformAPI := range api.Platform.Supported {
+		spec.Run(t, "acceptance-analyzer/"+platformAPI.String(), testAnalyzerFunc(platformAPI.String()), spec.Parallel(), spec.Report(report.Terminal{}))
 	}
 }
 
