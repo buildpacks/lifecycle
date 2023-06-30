bedfb81ea9be0e27d7d2e2c12c17bd7b0e3dc404
Natalie Arellano <narellano@vmware.com>
2023-06-30 11:17:56 -0400

    Update matching logic when considering if two image names are equivalent to ignore the digest portion of the reference if present (for the purpose of selecting data from run.toml to add to the lifecycle metadata label i.e., “run image for rebase”)
    Signed-off-by: Natalie Arellano <narellano@vmware.com>

--- internal/name/ref.go
+++ internal/name/ref.go
@@ -1,10 +1,32 @@
 package name
 
-import "github.com/google/go-containerregistry/pkg/name"
+import (
+	"strings"
 
-func ParseMaybe(ref string) string {
-	if nameRef, err := name.ParseReference(ref); err == nil {
-		return nameRef.Name()
+	"github.com/google/go-containerregistry/pkg/name"
+)
+
+// ParseMaybe attempts to parse the provided reference as a GGCR `name.Reference`, returning a modified `reference.Name()` if parsing is successful.
+// Unlike GGCR's `reference.Name()`, `ParseMaybe` will strip the digest portion of the reference,
+// retaining the provided tag or adding a `latest` tag if no tag is provided.
+// This is to aid in comparing two references when we really care about image names and not about image digests,
+// such as when populating `files.RunImageForRebase` information on an exported image.
+func ParseMaybe(provided string) string {
+	toParse := provided
+	if hasDigest(provided) {
+		toParse = trimDigest(provided)
 	}
-	return ref
+	if ref, err := name.ParseReference(toParse); err == nil {
+		return ref.Name()
+	}
+	return provided
+}
+
+func hasDigest(ref string) bool {
+	return strings.Contains(ref, "@sha256:")
+}
+
+func trimDigest(ref string) string {
+	parts := strings.Split(ref, "@")
+	return parts[0]
 }
--- internal/name/ref_test.go
+++ internal/name/ref_test.go
@@ -16,47 +16,72 @@ func TestRef(t *testing.T) {
 func testRef(t *testing.T, when spec.G, it spec.S) {
 	when(".ParseMaybe", func() {
 		when("provided reference", func() {
-			when("invalid", func() {
-				it("returns the provided reference", func() {
-					got := name.ParseMaybe("!@#$")
-					h.AssertEq(t, got, "!@#$")
-				})
-			})
-
-			when("has implicit registry", func() {
-				it("returns the fully qualified reference", func() {
-					got := name.ParseMaybe("some-library/some-repo:latest")
-					h.AssertEq(t, got, "index.docker.io/some-library/some-repo:latest")
-				})
-			})
-
-			when("has implicit library", func() {
-				it("returns the provided reference", func() {
-					got := name.ParseMaybe("some.registry/some-repo:latest")
-					h.AssertEq(t, got, "some.registry/some-repo:latest")
-				})
-
-				when("registry is docker.io", func() {
-					it("returns the fully qualified reference", func() {
-						got := name.ParseMaybe("index.docker.io/some-repo:latest")
-						h.AssertEq(t, got, "index.docker.io/library/some-repo:latest")
+			type testCase struct {
+				condition string
+				provided  string
+				does      string
+				expected  string
+			}
+			testCases := []testCase{
+				{
+					condition: "is invalid",
+					provided:  "!@#$",
+					does:      "returns the provided reference",
+					expected:  "!@#$",
+				},
+				{
+					condition: "has an implicit registry",
+					provided:  "some-library/some-repo:some-tag",
+					does:      "adds an explicit registry",
+					expected:  "index.docker.io/some-library/some-repo:some-tag",
+				},
+				{
+					condition: "has an implicit library",
+					provided:  "some.registry/some-repo:some-tag",
+					does:      "returns the provided reference",
+					expected:  "some.registry/some-repo:some-tag",
+				},
+				{
+					condition: "has an implicit library and has registry index.docker.io",
+					provided:  "index.docker.io/some-repo:some-tag",
+					does:      "adds an explicit library",
+					expected:  "index.docker.io/library/some-repo:some-tag",
+				},
+				{
+					condition: "has an implicit tag",
+					provided:  "some.registry/some-library/some-repo",
+					does:      "adds an explicit tag",
+					expected:  "some.registry/some-library/some-repo:latest",
+				},
+				{
+					condition: "has an implicit tag and has a digest",
+					provided:  "some.registry/some-library/some-repo@sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+					does:      "adds an explicit tag and removes the digest",
+					expected:  "some.registry/some-library/some-repo:latest",
+				},
+				{
+					condition: "has an explicit tag",
+					provided:  "some.registry/some-library/some-repo:some-tag",
+					does:      "returns the provided reference",
+					expected:  "some.registry/some-library/some-repo:some-tag",
+				},
+				{
+					condition: "has an explicit tag and has a digest",
+					provided:  "some.registry/some-library/some-repo:some-tag@sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+					does:      "removes the digest",
+					expected:  "some.registry/some-library/some-repo:some-tag",
+				},
+			}
+			for _, tc := range testCases {
+				tc := tc
+				w := when
+				w(tc.condition, func() {
+					it(tc.does, func() {
+						actual := name.ParseMaybe(tc.provided)
+						h.AssertEq(t, actual, tc.expected)
 					})
 				})
-			})
-
-			when("has implicit tag", func() {
-				it("returns the fully qualified reference", func() {
-					got := name.ParseMaybe("some.registry/some-library/some-repo")
-					h.AssertEq(t, got, "some.registry/some-library/some-repo:latest")
-				})
-			})
-
-			when("is fully qualified", func() {
-				it("returns the provided reference", func() {
-					got := name.ParseMaybe("some.registry/some-library/some-repo:some-tag")
-					h.AssertEq(t, got, "some.registry/some-library/some-repo:some-tag")
-				})
-			})
+			}
 		})
 	})
 }
