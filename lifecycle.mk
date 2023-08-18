c72dc20a932a7e9a2fb300798ef07c99feac8178
Domenico Luciani <dluciani@vmware.com>
2023-08-18 22:15:35 +0200

    Add support for darwin-arm64 chipset (#1169)
    * Add support for darwin-arm64 chipset
    
    Signed-off-by: Domenico Luciani <dluciani@vmware.com>
    
    * Split darwin builder into a separate module extracting the code for a more generic use
    
    Signed-off-by: Domenico Luciani <dluciani@vmware.com>
    
    * Split darwin.mk to lifecycle.mk and launcher.mk
    
    Signed-off-by: Domenico Luciani <dluciani@vmware.com>
    
    * Update launcher.mk with a better naming
    
    Co-authored-by: Natalie Arellano <narellano@vmware.com>
    Signed-off-by: Domenico Luciani <dluciani@vmware.com>
    
    * Update lifecycle.mk with a better naming
    
    Co-authored-by: Natalie Arellano <narellano@vmware.com>
    Signed-off-by: Domenico Luciani <dluciani@vmware.com>
    
    * Adjusted called function
    
    Signed-off-by: Domenico Luciani <dluciani@vmware.com>
    
    ---------
    
    Signed-off-by: Domenico Luciani <dluciani@vmware.com>
    Co-authored-by: Natalie Arellano <narellano@vmware.com>

---
+++ lifecycle.mk
@@ -0,0 +1,11 @@
+define build_lifecycle
+	@echo "> Building lifecycle for $(TARGET)..."
+	$(GOENV) $(GOBUILD) -o $(OUT_DIR)/lifecycle -a ./cmd/lifecycle
+	@echo "> Creating lifecycle symlinks for $(1)..."
+	ln -sf lifecycle $(OUT_DIR)/detector
+	ln -sf lifecycle $(OUT_DIR)/analyzer
+	ln -sf lifecycle $(OUT_DIR)/restorer
+	ln -sf lifecycle $(OUT_DIR)/builder
+	ln -sf lifecycle $(OUT_DIR)/exporter
+	ln -sf lifecycle $(OUT_DIR)/rebaser
+endef
